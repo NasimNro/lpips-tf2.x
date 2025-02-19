6f08f021c3e9744faf1809a1f72dd3032710874a
Nasim Habash <stud-hana1022@izbs-tf-196.imp>
2025-02-19 12:58:30 +0100

    linux

--- models/lpips_tensorflow.py
+++ models/lpips_tensorflow.py
@@ -171,9 +171,18 @@ def linear_model(input_image_size):
         image_size = input_image_size // (2 ** ii)
 
         model_input = Input(shape=(channel, image_size, image_size), dtype='float32')
-        model_output = Dropout(rate=0.5, dtype='float32')(model_input)
-        model_output = Conv2D(filters=1, kernel_size=1, strides=1, use_bias=False, dtype='float32',
-                              data_format='channels_first', name=name)(model_output)
+        # Convert from NCHW to NHWC format
+        x = Permute((2, 3, 1))(model_input)
+        x = Dropout(rate=0.5, dtype='float32')(x)
+        x = Conv2D(filters=1, 
+                  kernel_size=1, 
+                  strides=1, 
+                  use_bias=False, 
+                  dtype='float32',
+                  data_format='channels_last', 
+                  name=name)(x)
+        # Convert back to NCHW format
+        model_output = Permute((3, 1, 2))(x)
         inputs.append(model_input)
         outputs.append(model_output)
 
